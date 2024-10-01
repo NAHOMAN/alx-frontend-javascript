12ee1229a3374756a1e82990e3f5ee09f436c67d
2NG CODER <nahomnigussie7@gmail.com>
2024-10-01 03:52:37 -0700

    getBudget object task

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,11 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
