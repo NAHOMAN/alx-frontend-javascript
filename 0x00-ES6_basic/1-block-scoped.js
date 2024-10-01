f751e5960468d07375c543fc133c42ea63498f9a
2NG CODER <nahomnigussie7@gmail.com>
2024-10-01 07:09:17 -0700

    block scoped task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;   // Use let for task2 as well
 
   if (trueOrFalse) {
-   const task = true;      // Update the existing variable
-   const task2 = false;    // Update the existing variable
+   let task = true;      // Update the existing variable
+   let task2 = false;    // Update the existing variable
   }
 
   return [task, task2];
 }
-
