9bf6b8817140a8e824ee99b18ce60debcbbe340e
2NG CODER <nahomnigussie7@gmail.com>
2024-10-02 05:15:29 -0700

    Handle multiple successful promises task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,16 @@
-import { uploadPhoto, createUser } from './utils.js';
+import uploadPhoto from './5-photo-reject';  // Adjust the path based on your project structure
+import createUser from './4-user-promise';     // Adjust the path based on your project structure
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const { body } = results[0];        // from uploadPhoto
-      const { firstName, lastName } = results[1]; // from createUser
-      console.log(`${body} ${firstName} ${lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default async function handleProfileSignup() {
+  try {
+    const photoResponse = await uploadPhoto('my-photo.jpg');  // Call the uploadPhoto function
+    const userResponse = await createUser('Guillaume', 'Salva'); // Call the createUser function
+
+    // Log the desired output
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    // Log error message in case of failure
+    console.error('Signup system offline');
+  }
 }
+
