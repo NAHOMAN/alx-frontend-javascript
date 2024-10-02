3de74a105dba5bfb362962a9476dfde058a530c6
2NG CODER <nahomnigussie7@gmail.com>
2024-10-02 05:09:47 -0700

    Keep every promise task

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,14 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    const success = true; // Simulate an API call
+    // Simulating an API call with a timeout
+    const success = true; // Change this to false to simulate a failure
 
-    if (success) {
-      resolve("Success!");
-    } else {
-      reject("Error");
-    }
+    setTimeout(() => {
+      if (success) {
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    }, 1000); // Simulate a 1 second delay
   });
 }
