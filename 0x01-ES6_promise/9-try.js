076121bba37364f798c64564aa622b23f28854ae
2NG CODER <nahomnigussie7@gmail.com>
2024-10-02 05:52:34 -0700

    Throw an error task

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,16 +1,16 @@
 export default function guardrail(mathFunction) {
-  const queue = []; // Create an array named queue
+  const queue = []; // Create an empty array named queue
 
   try {
-    // Execute the mathFunction and append the result to the queue
+    // Execute the mathFunction and store the result
     const result = mathFunction();
-    queue.push(result); // Append the returned value to the queue
+    queue.push(result); // Append the result to the queue if successful
   } catch (error) {
-    // If an error occurs, append the error message to the queue
-    queue.push(error.message); // Append the error message to the queue
+    // If an error occurs, push the error message to the queue
+    queue.push(error.toString());
   } finally {
-    // Always append the guardrail message
-    queue.push('Guardrail was processed'); // Append the guardrail message
+    // Append the guardrail processed message regardless of success or failure
+    queue.push('Guardrail was processed');
   }
 
   return queue; // Return the queue
