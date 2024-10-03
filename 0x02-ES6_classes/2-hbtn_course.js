4001f5661c4f231e55ed03438bd9bae067df8fc4
2NG CODER <nahomnigussie7@gmail.com>
2024-10-03 03:23:22 -0700

    class named HolbertonCourse task

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,60 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Validate the type of name (should be a string)
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    // Validate the type of length (should be a number)
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    // Validate the type of students (should be an array of strings)
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    // Assign the validated attributes to underscore-prefixed properties
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name (with validation)
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length (with validation)
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students (with validation)
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
