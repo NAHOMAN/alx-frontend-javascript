64e1c26b0cceae71d0a050e401c5bd4177d63237
2NG CODER <nahomnigussie7@gmail.com>
2024-10-03 03:09:48 -0700

    class named task

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // Assign the maxStudentsSize parameter to the private property _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
