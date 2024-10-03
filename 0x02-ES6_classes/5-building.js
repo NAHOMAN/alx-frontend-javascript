9718e14d9916cc96394a5ce9738da35fdd3fc46a
2NG CODER <nahomnigussie7@gmail.com>
2024-10-03 03:36:31 -0700

    class named Building task

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,22 @@
-export default class Building {
-  constructor(sqft) {
-    // Validate that sqft is a number
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Square footage must be a number');
-    }
-    // Assign the validated attribute to underscore-prefixed property
-    this._sqft = sqft;
+import Building from './Building.js';
 
-    // Ensure that this class cannot be instantiated directly
-    if (new.target === Building) {
-      throw new Error('Building class cannot be instantiated directly');
-    }
+class OfficeBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors; // Additional property for the subclass
   }
 
-  // Getter for sqft
-  get sqft() {
-    return this._sqft;
-  }
-
-  // Method that must be implemented by any class extending Building
+  // Implementing the abstract method
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    return `Evacuate the ${this._floors} floors of the building!`;
   }
 }
+
+// Testing the subclass
+const office = new OfficeBuilding(5000, 10);
+console.log(office.sqft); // Output: 5000
+console.log(office.evacuationWarningMessage()); // Output: Evacuate the 10 floors of the building!
+
+// Uncommenting the following line will throw an error
+// const building = new Building(3000); // Error: Building class cannot be instantiated directly
+
