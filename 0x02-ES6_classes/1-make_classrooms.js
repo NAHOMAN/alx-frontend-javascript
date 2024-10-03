b7d11d7ab89f16d60915457746bf6896b43f5967
2NG CODER <nahomnigussie7@gmail.com>
2024-10-03 03:16:59 -0700

    import class from  task

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// Import ClassRoom class from 0-classroom.js
+import ClassRoom from './0-classroom.js';
+
+// Implement the initializeRooms function
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19), // First classroom with size 19
+    new ClassRoom(20), // Second classroom with size 20
+    new ClassRoom(34), // Third classroom with size 34
+  ];
+}
