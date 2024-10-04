1e501e19ed32f9e81b526a357769bb6ef481a512
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 02:49:15 -0700

    iterateThroughObject task

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  // Use a while loop to iterate through the iterator
-  let result = reportWithIterator.next(); // Get the first result
-
-  // Continue until all employees have been iterated
-  while (!result.done) {
-    console.log(result.value); // Log the current employee
-    result = reportWithIterator.next(); // Get the next employee
-  }
+  // Convert the iterator object to an array
+  const employees = [...reportWithIterator];
+  
+  // Join all the employee names using the separator "|"
+  return employees.join(' | ');
 }
-
