0d4d92f59f01028fc486e727575d9de558f532ee
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 05:31:49 -0700

    class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,6 @@
 class Building {
   constructor(sqft) {
-    // Check if sqft is a number and greater than zero
-    if (typeof sqft !== 'number' || sqft <= 0) {
-      throw new Error('sqft must be a positive number');
-    }
-    this._sqft = sqft; // Store sqft as a private attribute
+    this._sqft = sqft; // Store sqft in a private variable
   }
 
   // Getter for sqft
@@ -12,10 +8,10 @@ class Building {
     return this._sqft;
   }
 
-  // This method should be implemented by subclasses
+  // Abstract method to enforce subclass implementation
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
 
-export default Building; // Export the Building class for use in other modules
+export default Building;
