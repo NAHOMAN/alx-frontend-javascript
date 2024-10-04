f686e46a22df52809013e1eea6cd72b0294d46e7
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 04:06:40 -0700

    return promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,16 @@
 // Importing the necessary functions from utils.js
 import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-  // Using Promise.all to resolve both promises
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResult, userResult] = results;
+export default async function handleProfileSignup() {
+  try {
+    // Await both promises to resolve
+    const photo = await uploadPhoto();
+    const user = await createUser();
 
-      // Log the required values (photoResult.body, userResult.firstName, userResult.lastName)
-    })
-    .catch(() => {
-      // In case of an error, log "Signup system offline"
-      console.log('Signup system offline');
-    });
+    // Log the results
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    // Log error message if any promise fails
+    console.log('Signup system offline');
+  }
 }
