0907fc2d2a3c21f592e71f99952eefaa444685d3
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 03:25:33 -0700

    iterateThroughObject task

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,7 @@
-export default function createIteratorObject(report) {
-  const employees = Object.values(report.allEmployees); // Get the arrays of employees from the report
-  const flattenedEmployees = employees.flat(); // Flatten the array of arrays
-
-  // Use a generator function to create the iterator
-  function* employeeIterator() {
-    for (const employee of flattenedEmployees) {
-      yield employee; // Yield each employee one by one
+export default function* createIteratorObject(report) {
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      yield employee;
     }
   }
-
-  return employeeIterator(); // Return the generator function
 }
