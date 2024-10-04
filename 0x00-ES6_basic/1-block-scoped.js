7cf55ab824e9cbbc9a499c73642a982918c06c55
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 03:40:36 -0700

    Block Scope task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped variable
-    let task2 = false; // Block-scoped variable
+    const task = true;  // Block-scoped variable
+    const task2 = false; // Block-scoped variable
   }
 
   return [task, task2];
