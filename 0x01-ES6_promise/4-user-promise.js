6da6537744d7885641fd586e98399386d0774712
2NG CODER <nahomnigussie7@gmail.com>
2024-10-04 04:29:45 -0700

    -user-promise task

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,12 @@
 function signUpUser(firstName, lastName) {
   // Return a resolved promise with the user object
-  return {
-	  firstName:'value'
-	  lastName:' value'
-  }
-};
+  return Promise.resolve({
+    firstName: firstName,
+    lastName: lastName,
+  });
+}
+
+// Example usage
+signUpUser('bob', 'Dylan').then(user => {
+  console.log(user); // { firstName: 'bob', lastName: 'Dylan' }
+});
