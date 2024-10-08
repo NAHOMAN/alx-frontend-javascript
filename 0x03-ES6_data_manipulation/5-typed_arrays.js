d284d2e29c72e2144a6eb3ec42ce509c6644d865
2NG CODER <nahomnigussie7@gmail.com>
2024-10-08 04:51:42 -0700

    returns a new ArrayBuffer with an Int8

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,16 +1,21 @@
 export default function createInt8TypedArray(length, position, value) {
+  // Create a new ArrayBuffer of the specified length
+  const buffer = new ArrayBuffer(length);
+
   // Check if position is within the valid range
-  if (position < 0 || position >= length) {
+  if (position < 0 || position >= buffer.byteLength) {
     throw new Error('Position outside range');
   }
 
-  // Create a new ArrayBuffer of the specified length
-  const buffer = new ArrayBuffer(length);
   // Create a view of the buffer as an Int8Array
   const int8View = new Int8Array(buffer);
 
-  // Set the value at the specified position
-  int8View[position] = value;
+  // Using a try-catch block for setting the value
+  try {
+    int8View[position] = value;
+  } catch (error) {
+    throw new Error('Position outside range');
+  }
 
   return int8View;
 }
