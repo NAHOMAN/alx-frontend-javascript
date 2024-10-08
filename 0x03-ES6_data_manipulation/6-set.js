00bded272ed042ff8fd824545fe303dc5499fd78
2NG CODER <nahomnigussie7@gmail.com>
2024-10-08 04:55:53 -0700

    returns a Set from an array

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -2,8 +2,7 @@ export default function setFromArray(array) {
   // Check if the argument is an array
   if (!Array.isArray(array)) {
     throw new Error('Argument must be an array');
-  }
-  
+  } 
   // Return a Set created from the array
   return new Set(array);
 }
