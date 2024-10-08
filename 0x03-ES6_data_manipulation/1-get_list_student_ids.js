a50dbec318af13556644a4987ccf8c0bc896b6dc
2NG CODER <nahomnigussie7@gmail.com>
2024-10-08 07:08:51 -0700

    returns an array of ids from a list of object.

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -8,7 +8,7 @@ export default function getListStudentIds(students) {
   return students.reduce((acc, student) => {
     // Push the student's id into the accumulator array
     acc.push(student.id);
-
-	  return students.map(student => student.id);
+	  return acc;
   }, []);
+	return students.map(student => student.id);
 }
