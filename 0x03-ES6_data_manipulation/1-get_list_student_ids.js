c98f3be4284f73304012d585a4b1ae4eee710be4
2NG CODER <nahomnigussie7@gmail.com>
2024-10-08 04:35:39 -0700

    returns an array of ids from a list of object

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,9 @@
-function getListStudentIds(students) {
+export default function getListStudentIds(students) {
+  // Check if the argument is an array
   if (!Array.isArray(students)) {
     return [];
   }
+
+  // Use the map function to return an array of ids
   return students.map(student => student.id);
 }
-export default getListStudentIds;
